eed0cf4d209f27ad5add29654f5079a6f43b7ecf
shekhmohibur <mohibur.rahman2003@gmail.com>
2025-05-26 00:18:44 +0600

    successfully included all pets design

--- js/index.js
+++ js/index.js
@@ -42,15 +42,32 @@ const displayAllPets = (allPets) => {
         cartContainer.innerHTML =`
             <figure class="px-10 pt-10">
                 <img
-                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                src="${pet.image}"
                 alt="Shoes"
                 class="rounded-xl" />
             </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions">
-                <button class="btn btn-primary">Buy Now</button>
+            <div class="card-body">
+                <h2 class="card-title text-2xl font-bold">${pet.pet_name}<h2>
+                <div class="flex items-center gap-2">
+                    <img src="https://img.icons8.com/?size=100&id=jQkL4uOqbTW7&format=png&color=838383" class="w-7">
+                    <h2 class="text-lg text-gray-500">Breed: ${pet.breed == undefined?'No Info':pet.breed}</h2>
+                </div>
+                <div class="flex items-center gap-2">
+                    <img src="https://img.icons8.com/?size=100&id=85102&format=png&color=838383" class="w-7">
+                    <h2 class="text-lg text-gray-500">Birth: ${pet.date_of_birth == undefined?'No Info':pet.date_of_birth}</h2>
+                </div>
+                <div class="flex items-center gap-2">
+                    <img src="https://img.icons8.com/?size=100&id=70834&format=png&color=838383" class="w-7">
+                    <h2 class="text-lg text-gray-500">Gender: ${pet.gender == undefined?'No Info':pet.gender}</h2>
+                </div>
+                <div class="flex items-center gap-2">
+                    <img src="https://img.icons8.com/?size=100&id=85843&format=png&color=838383" class="w-7">
+                    <h2 class="text-lg text-gray-500">Price: ${pet.price == undefined?'No Info':pet.price}</h2>
+                </div>
+                <div class="flex justify-between px-3 pt-3">
+                <i class="btn pt-2 hover:bg-[#0E7A81] fa-regular fa-thumbs-up text-2xl hover:text-white"></i>
+                <button class="btn px-3 text-[#0E7A81] text-lg hover:bg-[#0E7A81] hover:text-white">Adopt</button>
+                <button class="btn px-3 text-[#0E7A81] text-lg hover:bg-[#0E7A81] hover:text-white">Details</button>
                 </div>
             </div>
         `;
