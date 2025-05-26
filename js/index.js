71dbe6d27701df551f0bb010d2fd482afe7bf706
shekhmohibur <mohibur.rahman2003@gmail.com>
2025-05-26 09:52:56 +0600

    modal added for details

--- js/index.js
+++ js/index.js
@@ -18,6 +18,13 @@ const specificPetsCarts = async(category) => {
     displayAllPets(data.data)
     
 }
+// loading pets details based on pets id
+const detailsPetsCarts = async(petId) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/peddy/pet/${petId}`);
+    const data = await res.json();
+    displayPetsBio(data.petData)
+    
+}
 
 petCategory();
 allPetsCarts();
@@ -45,7 +52,6 @@ const displayCategoryBtn = (category) => {
 //displaying thumbnail after click like button
 const showThumbnail = (thumbnails) => {
     const likedPets = document.getElementById('likedPets');
-    console.log(thumbnails)
     thumbnails.forEach(thumbnail => {
     const displayThumbnail = document.getElementById(`show-thumbnail-${thumbnail.petId}`);
     displayThumbnail.addEventListener('click', () => {
@@ -62,6 +68,59 @@ const showThumbnail = (thumbnails) => {
         likedPets.append(thumbnailContainer)
     })
     })
+}
+/*<div class="modal-box">
+            <h3 class="text-lg font-bold">Hello!</h3>
+            <p class="py-4">Press ESC key or click the button below to close</p>
+            <div class="modal-action">
+            <form method="dialog">
+                <button class="btn">Close</button>
+            </form>
+            </div>
+        </div> */
+//displaying pets infromation in modal
+const displayPetsBio = (details) => {
+    document.getElementById('adoptModal').showModal();
+    document.getElementById('modalDetails').innerHTML = `
+        <div>
+            <img src="${details.image}" class="rounded-xl w-full">
+        </div>
+        <div>
+            <h2 class="text-2xl font-bold">${details.pet_name}</h2>
+            <div class="flex items-center gap-2">
+                    <img src="https://img.icons8.com/?size=100&id=jQkL4uOqbTW7&format=png&color=838383" class="w-7">
+                    <h2 class="text-lg text-gray-500">Breed: ${details.breed == undefined?'No Info':details.breed}</h2>
+                </div>
+                <div class="flex items-center gap-2">
+                    <img src="https://img.icons8.com/?size=100&id=85102&format=png&color=838383" class="w-7">
+                    <h2 class="text-lg text-gray-500">Birth: ${details.date_of_birth == undefined?'No Info':details.date_of_birth}</h2>
+                </div>
+                <div class="flex items-center gap-2">
+                    <img src="https://img.icons8.com/?size=100&id=70834&format=png&color=838383" class="w-7">
+                    <h2 class="text-lg text-gray-500">Gender: ${details.gender == undefined?'No Info':details.gender}</h2>
+                </div>
+                <div class="flex items-center gap-2">
+                    <img src="https://img.icons8.com/?size=100&id=70834&format=png&color=838383" class="w-7">
+                    <h2 class="text-lg text-gray-500">Vaccinated: ${details.vaccinated_status == undefined?'No Info':details.vaccinated_status}</h2>
+                </div>
+                <div class="flex items-center gap-2">
+                    <img src="https://img.icons8.com/?size=100&id=85843&format=png&color=838383" class="w-7">
+                    <h2 class="text-lg text-gray-500">Price: ${details.price == undefined?'No Info':details.price}</h2>
+                </div>
+        </div>
+        <div>
+            <h2 class="text-lg font-semibold">Details Information</h2>
+            <div class="flex items-center gap-2">
+                    <p class=" text-gray-500">Price: ${details.pet_details == undefined?'No Info':details.pet_details}</p>
+                </div>
+        </div>
+        <div>
+            <form method="dialog" class="w-full">
+                <button class="btn w-full bg-[#0e79813b] text-lg text-[#0e7a81]">Close</button>
+            </form>
+        </div>
+    `;
+    
 }
 // displaying pets carts
 const displayAllPets = (allPets) => {
@@ -111,7 +170,7 @@ its layout. The point of using Lorem Ipsum is that it has a.</p>
                 </div>
                 <div class="flex justify-between px-3 pt-3">
                 <i id="show-thumbnail-${pet.petId}" class="btn pt-2 hover:bg-[#0E7A81] fa-regular fa-thumbs-up text-2xl hover:text-white"></i>
-                <button class="btn px-3 text-[#0E7A81] text-lg hover:bg-[#0E7A81] hover:text-white">Adopt</button>
+                <button onclick="detailsPetsCarts(${pet.petId})" class="btn px-3 text-[#0E7A81] text-lg hover:bg-[#0E7A81] hover:text-white">Adopt</button>
                 <button class="btn px-3 text-[#0E7A81] text-lg hover:bg-[#0E7A81] hover:text-white">Details</button>
                 </div>
             </div>
@@ -119,7 +178,3 @@ its layout. The point of using Lorem Ipsum is that it has a.</p>
         petsCart.append(cartContainer);
     })
 }
-
-    document.getElementById('adopt-modal').addEventListener('click', (event) =>{
-        event.target.showModal()
-    })
